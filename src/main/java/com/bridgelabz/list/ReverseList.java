7a4a06202a6882c150d9cf9f5d52a15a9361da8f
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-28 11:44:13 +0530

    [Sehajpreet] Add: adding all the file

---
+++ src/main/java/com/bridgelabz/list/ReverseList.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.list;
+import java.util.*;
+
+public class ReverseList {
+    public static void reverseList(List<Integer> list) {
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            int temp = list.get(left);
+            list.set(left, list.get(right));
+            list.set(right, temp);
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        System.out.println("Original List: " + list);
+        reverseList(list);
+        System.out.println("Reversed List: " + list);
+
+        scanner.close();
+    }
+}
